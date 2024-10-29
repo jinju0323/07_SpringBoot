628589afd2d2629238d9ed4ab78132a192110b94
jinju <tlawlswn28@naver.com>
2024-10-29 15:57:45 +0900

    09 RestfulAPI | 간단한 json 출력 및 Ajax 메일 발송 연습

---
+++ 09_restfulapi/src/main/java/kr/jinju/restfulapi/controllers/MailWriteController.java
@@ -0,0 +1,21 @@
+package kr.jinju.restfulapi.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class MailWriteController {
+    /**
+     * 메일 발송 폼을 출력하는 메서드
+     * 웹 브라우저에게 HTML을 전송한다
+     * 프론트엔드 역할
+     * 
+     * @return - 메일 발송 폼
+     */
+    @GetMapping("/mail_form")
+    public String mailForm() {
+        return "mail_form.html";
+    }
+    
+}
