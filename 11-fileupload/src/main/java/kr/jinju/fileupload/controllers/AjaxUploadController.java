50cb6f2d26a86b2797e7ad3a758bd57b351e97d2
jinju <tlawlswn28@naver.com>
2024-11-06 14:54:07 +0900

    11 FileUpload | 파일업로드 Ajax구현
    Ajax 준비 : Controller,  View
    Axios 구현 : JS 작업
    이후 AxiosHelper 생성해야함

---
+++ 11-fileupload/src/main/java/kr/jinju/fileupload/controllers/AjaxUploadController.java
@@ -0,0 +1,14 @@
+package kr.jinju.fileupload.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class AjaxUploadController {
+    @GetMapping("/ajax/upload")
+    public String upload() {
+        return "ajax/upload";
+    }
+    
+}
---
+++ 11-fileupload/src/main/java/kr/jinju/fileupload/controllers/apis/UploadRestController.java
@@ -0,0 +1,45 @@
+package kr.jinju.fileupload.controllers.apis;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import kr.jinju.fileupload.helpers.FileHelper;
+import kr.jinju.fileupload.helpers.RestHelper;
+import kr.jinju.fileupload.models.UploadItem;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+public class UploadRestController {
+    @Autowired
+    private RestHelper restHelper;
+
+    @Autowired
+    private FileHelper fileHelper;
+
+    @PostMapping("/api/upload_ok")
+    public Map<String, Object> uploadOk(Model model,
+            @RequestParam(value = "profile-photo", required = false) MultipartFile profilePhoto) {
+        
+        UploadItem item = null;
+
+        try {
+            item = fileHelper.saveMultipartFile(profilePhoto);
+        } catch (Exception e) {
+            return restHelper.serverError(e);
+        }
+
+        Map<String, Object> data = new LinkedHashMap<String, Object>();
+        data.put("item", item);
+
+        return restHelper.sendJson(data);
+    }
+    
+}
