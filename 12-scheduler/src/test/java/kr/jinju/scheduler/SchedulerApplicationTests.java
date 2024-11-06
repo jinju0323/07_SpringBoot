c9cd1146bfd868077c9e0264de716852a613f44e
jinju0323 <tlawlswn28@naver.com>
2024-11-06 16:08:33 +0900

    12 Scheduler | Scheduler기능 사용해서 자동으로 시간 정보 기록 log 출력

---
+++ 12-scheduler/src/test/java/kr/jinju/scheduler/SchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.jinju.scheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
